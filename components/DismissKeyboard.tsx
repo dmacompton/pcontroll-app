4bc069b615167aaea180f3f50ae1e3050cf89382
dmacompton <dmacompton@gmail.com>
2019-04-29 22:42:47 +0300

    Add DismissKeyboard component

---
+++ components/DismissKeyboard.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import {
+  Keyboard,
+  TouchableWithoutFeedback
+} from "react-native";
+
+interface Props {
+  children?: Array<React.ReactChild> | React.ReactChild;
+}
+
+const DismissKeyboard = ({ children }: Props) => (
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    {children}
+  </TouchableWithoutFeedback>
+);
+
+export default DismissKeyboard;
